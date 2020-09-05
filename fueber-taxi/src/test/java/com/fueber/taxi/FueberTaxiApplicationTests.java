8b063ba6389d14520d49b1343fd53c1eb2853864
Narendran Arumugavadivelu <narendran.arumugavadivelu@gmail.com>
2020-09-05 21:26:46 +0530

    Started with JUnit test cases

--- fueber-taxi/src/test/java/com/fueber/taxi/FueberTaxiApplicationTests.java
+++ fueber-taxi/src/test/java/com/fueber/taxi/FueberTaxiApplicationTests.java
@@ -1,13 +1,30 @@
 package com.fueber.taxi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootTest
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.fueber.taxi.dto.CustomerDTO;
+import com.fueber.taxi.dto.TaxiDTO;
+
+@RunWith(MockitoJUnitRunner.class)
 class FueberTaxiApplicationTests {
+	
+	private List<TaxiDTO> availableTaxiDTOList = new ArrayList<>();
+	
+	private List<CustomerDTO> customerDetailsList = new ArrayList<>();
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	public void initializeMockito() {
+		MockitoAnnotations.initMocks(this);
+		
+		availableTaxiDTOList.add(new TaxiDTO("KA04MG4516", 12.979270, 77.571680, false, false));
 	}
-
+	
+	
+	
 }
